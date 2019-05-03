d39b21ce8b52d64c9803abf21d376bd18434e6ed
Mohanakrishnan Ashokan <M1028218@mindtree.com>
2019-05-03 15:17:45 +0530

    Removed default test cases from the components

--- PocUI/src/app/public/components/global-error/global-error.component.spec.ts
+++ PocUI/src/app/public/components/global-error/global-error.component.spec.ts
@@ -5,20 +5,4 @@ import { GlobalErrorComponent } from './global-error.component';
 describe('GlobalErrorComponent', () => {
   let component: GlobalErrorComponent;
   let fixture: ComponentFixture<GlobalErrorComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [GlobalErrorComponent]
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GlobalErrorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
