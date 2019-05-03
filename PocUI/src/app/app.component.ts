10b2b62286adf703d8e3320e893b50fa51408b61
Mohanakrishnan Ashokan <M1028218@mindtree.com>
2019-05-03 12:09:06 +0530

    Resolved Errors

--- PocUI/src/app/app.component.ts
+++ PocUI/src/app/app.component.ts
@@ -1,10 +1,8 @@
+import { CardService } from './public/services/card.service';
+import { Card } from './public/models/card.model';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'om './core/models/card.model';
-import { Router, Navapp/public/components/register/node_modules/rxjs';
+import { Observable } from 'rxjs';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { CardService } from '.service';
-import { Card } frce';
-import { Card } from 'from './core/services/cardel';
 
 @Component({
   selector: 'app-root',
@@ -16,21 +14,16 @@ export class AppComponent implements OnInit {
   imgSrc: SafeResourceUrl;
   cards$: Observable<Card[]>;
 
-  constructor(import { CardService } from './core/services/card.service';
-impo
-    protected sanitizer: DomSani==
-t===izer,
-    private cardSvc:  { Card } from './public/models/card.model';
-==CarService
+  constructor(
+    protected sanitizer: DomSanitizer,
+    private cardSvc: CardService
   ) {}
 
   ngOnInit() {
     this.title = 'Angular App';
-    this.imgSrt { CardService } from './public/services/card.service';
-importrc = this.sanitizer.bypassSecurityTrustResourceUrl(
-      'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xemrt { CardService } from './public/services/card.service';
-import { Card } from './public/models/card.model';
-=======
-import { CardService } from './core/services/card.service';
-import { Card } from './core/models/card.model';
-import { Router, Nav      'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem
+    this.imgSrc = this.sanitizer.bypassSecurityTrustResourceUrl(
+      'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg=='
+    );
+    this.cards$ = this.cardSvc.getCards();
+  }
+}
